5578a09ac152cb6183edeb2d6c1de0bdf0d701d9
Cole <yoshi88639@gmail.com>
2025-04-02 01:24:49 -0700

    fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: ["var(--font-urbanist)"],
+      },
+      colors: {
+        nsu: {
+          "red-100": "#A02732",
+          "red-200": "#70161E",
+          "red-300": "#3B252C",
+          "gray-100": "#FFFFFF",
+          "gray-200": "#8F8389",
+          "gray-300": "#000000",
+          "tan-100": "#E7DFD4",
+        },
+      },
+    },
   },
   plugins: [],
 };
