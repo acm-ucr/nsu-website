fea64b8a476599800542dfb4553de2c4b39ee135
Peyton Chen <pchen215@ucr.edu>
2025-04-09 12:50:02 -0700

    Add example component

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,12 +1,12 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="flex flex-col w-2/3 items-center gap-5 bg-red-200 p-10">
+      <div className="w-1/2 bg-blue-200 p-5 text-center text-2xl">{props.text1}</div>
+      <div className="flex w-1/2 flex-row gap-5">
+        <div className="w-full bg-blue-400 p-5 text-center text-2xl rounded-full">{props.text2}</div>
+        <div className="w-full bg-blue-400 p-5 text-center text-2xl rounded-full">{props.text2}</div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="w-1/2 bg-blue-700 p-5 text-center text-white text-2xl">{props.text3}</div>
     </div>
   );
 };
