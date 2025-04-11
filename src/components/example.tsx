3d1f4a63813f42d2fa7ed4ddd4f6d20bd2963a50
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2025-04-11 00:17:36 -0700

    Join Us Button

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,10 +1,14 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex flex-col text-2xl text-center rounded-lg w-3/4 items-center gap-10 bg-red-200 p-10">
+    <div className="flex w-3/4 flex-col items-center gap-10 rounded-lg bg-red-200 p-10 text-center text-2xl">
       <div className="w-1/2 bg-blue-200 py-5">{props.text1}</div>
       <div className="flex w-1/2 flex-row gap-10">
-        <div className="w-full bg-blue-400 py-5 rounded-full">{props.text2}</div>
-        <div className="w-full bg-blue-400 py-5 rounded-full">{props.text2}</div>
+        <div className="w-full rounded-full bg-blue-400 py-5">
+          {props.text2}
+        </div>
+        <div className="w-full rounded-full bg-blue-400 py-5">
+          {props.text2}
+        </div>
       </div>
       <div className="w-1/2 bg-blue-500 py-5 text-white">{props.text3}</div>
     </div>
