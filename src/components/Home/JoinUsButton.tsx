bd2bd329c4f2bdace29492bb4a8c186c358b486a
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2025-04-15 15:35:41 -0700

    minor fixes

--- src/components/Home/JoinUsButton.tsx
+++ src/components/Home/JoinUsButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const JoinUsButton = () => {
   return (
-    <div className="bg-nsu-red-200 flex w-full items-center rounded-lg p-3 duration-150 motion-safe:hover:scale-110">
+    <div className="bg-nsu-red-200 font-urbanist flex w-full items-center rounded-[22px] p-3 duration-150 motion-safe:hover:scale-110">
       <Link href="/JOINNSU" className="text-3xl font-bold text-white">
         JOIN US
       </Link>
