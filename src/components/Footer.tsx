483a39b7f28c525e25ea33d87bd01a837dd7f03c
rishikpulipaka <rpuli007@ucr.edu>
2025-04-15 20:50:32 -0700

    formatting changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/assets/logo2.webp";
 import footerBG from "@/public/assets/footer-background.webp";
-import { FaInstagram, FaDiscord, FaLinkedinIn, FaTiktok, FaYoutube } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaDiscord,
+  FaLinkedinIn,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
 import { SiLinktree } from "react-icons/si";
 import { FiMail } from "react-icons/fi";
 
@@ -13,27 +19,24 @@ const Footer = () => {
         <Image
           src={footerBG}
           alt="Background silhouette"
-          className="w-full h-full object-cover object-center"
+          className="h-full w-full object-cover object-center"
         />
       </div>
-      <div className="relative bg-nsu-gray-300 bg-opacity-80 text-nsu-gray-100 py-6 px-4 sm:px-8">
-        <div className="max-w-8xl mx-auto flex flex-col md:flex-row justify-between items-center">
-          <div className="flex items-center space-x-3 mb-4 md:mb-0">
-            <Image
-              src={logo}
-              alt="UCN NSU Logo"
-              className="h-auto w-auto"
-            />
+      <div className="bg-nsu-gray-300 bg-opacity-80 text-nsu-gray-100 relative px-4 py-6 sm:px-8">
+        <div className="max-w-8xl mx-auto flex flex-col items-center justify-between md:flex-row">
+          <div className="mb-4 flex items-center space-x-3 md:mb-0">
+            <Image src={logo} alt="UCN NSU Logo" className="h-auto w-auto" />
           </div>
 
           <div className="text-center md:text-right">
             <h2 className="text-xl font-semibold">CONTACT US!</h2>
-            <p className="text-sm mt-1">
-              900 University Ave.<br />
+            <p className="mt-1 text-sm">
+              900 University Ave.
+              <br />
               Riverside, CA 92521
             </p>
 
-            <div className="flex justify-center md:justify-end space-x-4 mt-3">
+            <div className="mt-3 flex justify-center space-x-4 md:justify-end">
               <Link href="/" target="_blank">
                 <FaDiscord />
               </Link>
