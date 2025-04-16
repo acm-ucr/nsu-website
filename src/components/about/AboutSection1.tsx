7bb06ba88f6cd0f2aed96aef409ca8971101e564
BestinMW <bestin.musashi.watts@gmail.com>
2025-04-16 01:15:03 -0700

    formatted

--- src/components/about/AboutSection1.tsx
+++ src/components/about/AboutSection1.tsx
@@ -5,17 +5,28 @@ const AboutSection1 = () => {
   return (
     <div>
       <div className="bg-nsu-red-200 my-2 h-[4px] w-1/2"></div>
-    <div>
-      <p className="font-nsu-main w-3/4 mt-5 mx-8 text-nsu-gray-300 text-base leading-5 tracking-normal">
-        Nikkei Student Union (NSU) is a socio-cultural organization devoted to promoting, preserving, and educating the surrounding community and college-aged youth about the history of Japanese and Japanese Americans through cultural events and social activities.
-      </p>
-    </div>
-    <div className="mt-7">
-      <div className="flex justify-center mx-auto w-1/2 relative">
-        <Image className="z-0 absolute opacity-50 mt-2 ml-4" src={AboutImg2} alt="About1 Image Background"/>
-        <Image className="z-1 opacity-100 mb-9" src={AboutImg2} alt="About1 Image"/>
+      <div>
+        <p className="font-nsu-main text-nsu-gray-300 mx-8 mt-5 w-3/4 text-base leading-5 tracking-normal">
+          Nikkei Student Union (NSU) is a socio-cultural organization devoted to
+          promoting, preserving, and educating the surrounding community and
+          college-aged youth about the history of Japanese and Japanese
+          Americans through cultural events and social activities.
+        </p>
       </div>
-    </div>
+      <div className="mt-7">
+        <div className="relative mx-auto flex w-1/2 justify-center">
+          <Image
+            className="absolute z-0 mt-2 ml-4 opacity-50"
+            src={AboutImg2}
+            alt="About1 Image Background"
+          />
+          <Image
+            className="z-1 mb-9 opacity-100"
+            src={AboutImg2}
+            alt="About1 Image"
+          />
+        </div>
+      </div>
     </div>
   );
 };
