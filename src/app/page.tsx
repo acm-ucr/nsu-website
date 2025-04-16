71ba38f742f5587ee25c17e1dab82519209c80b2
Cole <yoshi88639@gmail.com>
2025-04-16 00:42:07 -0700

    Migrated to tailwindv4 completely and fixed a bunch of stuff. Also deleted exampele component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Landing from "@/components/Home/Landing";
 import Welcome from "@/components/Home/Welcome";
 import Offer from "@/components/Home/Offer";
 import AdditionalInfo from "@/components/Home/AdditionalInfo";
-import Example from "@/components/example";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
@@ -11,7 +10,6 @@ const Home = () => {
       <Offer />
       <AdditionalInfo />
       NSU website
-      <Example text1="Adjust" text2="these" text3="Props" />
     </div>
   );
 };
