3eb6f89fe5d9665db869285cd99f1f870cb5e153
rishikpulipaka <rpuli007@ucr.edu>
2025-04-15 20:49:04 -0700

    Made initial footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,8 @@ export default function RootLayout({ children }: LayoutProps) {
     <html lang="en">
       <body className={inter.className}>
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+
+        <Footer />
       </body>
     </html>
   );
