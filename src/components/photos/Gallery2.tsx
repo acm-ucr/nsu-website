d94864734e22d4453b225391eaf80d76e1f5246b
CadeJordan <cade.m.jordan@gmail.com>
2025-04-30 03:12:01 -0700

    Created gallery grid

--- src/components/photos/Gallery2.tsx
+++ src/components/photos/Gallery2.tsx
@@ -1,5 +1,28 @@
 const Gallery2 = () => {
-  return <div>Gallery Section 2</div>;
+  return (
+    <div>
+      <div className="ml-15 flex flex-col">
+        <div>
+          <p className="text-nsu-red-200 font-nsu-main text-2xl leading-9 font-bold tracking-normal">
+            Photos from some yr/event
+          </p>
+        </div>
+        <div className="bg-nsu-red-200 my-2 mr-auto h-[4px] w-3/5"></div>
+      </div>
+      <div className="mt-5 mb-5">
+        {[...Array(5)].map((_, rowIdx) => (
+          <div
+            key={rowIdx}
+            className={`mb-3 grid grid-cols-5 gap-3 ${rowIdx % 2 !== 0 ? "ml-15" : "mr-15"}`}
+          >
+            {[...Array(5)].map((_, i) => (
+              <div key={i} className="aspect-square bg-gray-400"></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery2;
