a29787029469eac87f8499099fed86396b602652
BestinMW <bestin.musashi.watts@gmail.com>
2025-04-30 13:50:48 -0700

    fixed eslint

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,6 @@
 import BoardGrid from "@/components/board/BoardGrid";
 import BoardTitleSection from "@/components/board/TitleSection";
-import BoardCard from "@/components/board/BoardCard";
-import presidentImage from "@/public/board/president.webp";
 import Header from "@/components/Header";
-
 
 export default function Page() {
   return (
