4557ec2ae19cecd316a06b6ed9e3fb865da0a7a9
CadeJordan <cade.m.jordan@gmail.com>
2025-04-30 15:57:58 -0700

    added most photos

--- src/components/photos/Gallery2.tsx
+++ src/components/photos/Gallery2.tsx
@@ -7,9 +7,39 @@ const gallery = [
   "/photos/CIMG1974.webp",
   "/photos/CIMG1980.webp",
   "/photos/CIMG1982.webp",
+  "/photos/CIMG1988.webp",
+  "/photos/CIMG2022.webp",
+  "/photos/CIMG2144.webp",
+  "/photos/CIMG2169.webp",
+  "/photos/CIMG2174.webp",
+  "/photos/CIMG2185.webp",
+  "/photos/DSC00045.webp",
+  "/photos/DSC00060.webp",
+  "/photos/DSC00082.webp",
+  "/photos/DSC00092.webp",
+  "/photos/DSC00114.webp",
+  "/photos/DSC00894.webp",
+  "/photos/DSC00912.webp",
+  "/photos/DSC00916.webp",
+  "/photos/DSC00942.webp",
+  "/photos/DSC00967.webp",
+  "/photos/DSC00987.webp",
+  "/photos/DSC01199.webp",
+  "/photos/DSC01783.webp",
+  "/photos/DSC01814.webp",
+  "/photos/fam_wars.webp",
+  "/photos/IMG_1008.webp",
+  "/photos/IMG_1025.webp",
+  "/photos/IMG_6175.webp",
+  "/photos/IMG_6328.webp",
+  "/photos/IMG_6339.webp",
+  "/photos/IMG_7395.webp",
 ];
 
 const Gallery2 = () => {
+  const imagesPerRow = 5;
+  const rows = Math.ceil(gallery.length / imagesPerRow);
+
   return (
     <div>
       <div className="ml-15 flex flex-col">
@@ -21,19 +51,24 @@ const Gallery2 = () => {
         <div className="bg-nsu-red-200 my-2 mr-auto h-[4px] w-3/5"></div>
       </div>
       <div className="mt-5 mb-5">
-        {[...Array(5)].map((_, rowIdx) => (
+        {[...Array(rows)].map((_, rowIdx) => (
           <div
             key={rowIdx}
             className={`mb-3 grid grid-cols-5 gap-3 ${rowIdx % 2 !== 0 ? "ml-15" : "mr-15"}`}
           >
-            {gallery.slice(rowIdx * 5, rowIdx * 5 + 5).map((src, i) => (
-              <img
-                key={i}
-                src={src}
-                alt={"Gallery image ${rowIdx * 5 + i+1}"}
-                className="aspect-square object-cover"
-              />
-            ))}
+            {gallery
+              .slice(
+                rowIdx * imagesPerRow,
+                rowIdx * imagesPerRow + imagesPerRow,
+              )
+              .map((src, i) => (
+                <img
+                  key={rowIdx * imagesPerRow + i}
+                  src={src}
+                  alt={`Gallery image ${rowIdx * imagesPerRow + i + 1}`}
+                  className="aspect-square object-cover"
+                />
+              ))}
           </div>
         ))}
       </div>
