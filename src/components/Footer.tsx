50b6c1831d5d3887950cc4c6f6dd8000026e2409
Pranay Thakur <pranaythakur543@gmail.com>
2025-05-01 02:19:25 -0700

    (feat)-footer image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/assets/logo2.webp";
+import footerImage from "@/public/assets/footer.svg";
 import {
   FaInstagram,
   FaDiscord,
@@ -14,14 +15,21 @@ import { FiMail } from "react-icons/fi";
 const Footer = () => {
   return (
     <footer className="relative w-full">
+      <div
+        className="h-115 w-full bg-top bg-repeat-x"
+        style={{
+          backgroundImage: `url(${footerImage.src})`,
+        }}
+      />
+
       <div className="bg-nsu-gray-300 bg-opacity-80 text-nsu-gray-100 relative px-4 py-6 sm:px-8">
         <div className="max-w-8xl mx-auto flex flex-col items-center justify-between md:flex-row">
           <div className="mb-4 flex items-center space-x-3 md:mb-0">
-            <Image src={logo} alt="UCN NSU Logo" className="h-auto w-auto" />
+            <Image src={logo} alt="UCR NSU Logo" className="h-auto w-auto" />
           </div>
 
           <div className="flex flex-col gap-4 text-center md:text-right">
-            <div className="">
+            <div>
               <h2 className="text-xl font-bold">CONTACT US!</h2>
               <p className="text-md mt-1">
                 900 University Ave.
