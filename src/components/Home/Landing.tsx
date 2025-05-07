1b9b82a0e42d0c2d65c9d882aa9342d212411cf6
Peyton Chen <pchen215@ucr.edu>
2025-05-07 12:54:05 -0700

    Start adding code

--- src/components/Home/Landing.tsx
+++ src/components/Home/Landing.tsx
@@ -6,44 +6,38 @@ const Landing = () => {
   return (
     <div className="flex w-full justify-center">
       <div className="relative flex w-2/3 flex-row">
-        <div className="absolute w-5/12 px-10">
+        <div className="absolute w-5/12 md:px-10">
           <Image src={Umbrella} alt="NSU Umbrella" />
         </div>
-        <div className="flex flex-col space-y-3 px-20 pt-20">
-          <div className="ml-30">
+        <div className="flex flex-col md:space-y-3 md:px-20 md:pt-20">
+          <div className="mt-10 ml-5 text-4xl font-extrabold md:ml-30 md:text-8xl">
             <h1
-              className="text-nsu-tan-100 absolute top-[0.87em] z-0 ml-1 text-8xl font-extrabold"
+              className="text-nsu-tan-100 absolute z-0 mt-[2px] ml-[2px] md:mt-1 md:ml-1"
               style={{ WebkitTextStroke: "1px #70161e" }}
             >
               Nikkei
             </h1>
-            <h1 className="text-nsu-red-200 relative z-10 text-8xl font-extrabold">
-              Nikkei
-            </h1>
+            <h1 className="text-nsu-red-200 relative z-10">Nikkei</h1>
           </div>
-          <div className="ml-60">
+          <div className="ml-15 text-4xl font-extrabold md:ml-60 md:text-8xl">
             <h1
-              className="text-nsu-tan-100 absolute top-[1.99em] z-0 ml-1 text-8xl font-extrabold"
+              className="text-nsu-tan-100 absolute z-0 mt-[2px] ml-[2px] md:mt-1 md:ml-1"
               style={{ WebkitTextStroke: "1px #70161e" }}
             >
               Student
             </h1>
-            <h1 className="text-nsu-red-200 relative z-10 text-8xl font-extrabold">
-              Student
-            </h1>
+            <h1 className="text-nsu-red-200 relative z-10">Student</h1>
           </div>
-          <div className="ml-110">
+          <div className="ml-35 text-4xl font-extrabold md:ml-110 md:text-8xl">
             <h1
-              className="text-nsu-tan-100 absolute top-[3.12em] z-0 ml-1 text-8xl font-extrabold"
+              className="text-nsu-tan-100 absolute z-0 mt-[2px] ml-[2px] md:mt-1 md:ml-1"
               style={{ WebkitTextStroke: "1px #70161e" }}
             >
               Union
             </h1>
-            <h1 className="text-nsu-red-200 relative z-10 text-8xl font-extrabold">
-              Union
-            </h1>
+            <h1 className="text-nsu-red-200 relative z-10">Union</h1>
           </div>
-          <div className="text-nsu-black-100 border-nsu-red-200 mt-10 border-b-4 py-2 text-center text-[1.68rem]">
+          <div className="text-nsu-black-100 border-nsu-red-200 mt-10 border-b-4 py-2 text-center md:text-[1.68rem]">
             NSU is a diverse Japanese socio-cultural club at UCR.
           </div>
         </div>
