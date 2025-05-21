73349a4c9ba52573d2fb873e119528b7a7da5085
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2025-05-20 17:52:51 -0700

    animate board page

--- src/components/board/BoardGrid.tsx
+++ src/components/board/BoardGrid.tsx
@@ -1,16 +1,23 @@
+"use client";
+
 import BoardCard from "@/components/board/BoardCard";
 import { BOARD } from "@/data/Board";
+import { motion } from "framer-motion";
 
 const BoardGrid = () => {
   return (
-    <div className="relative grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="relative grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
       {BOARD.map((item, index) => (
-        <BoardCard
+        <motion.div
           key={index}
-          image={item.image}
-          name={item.name}
-          role={item.role}
-        />
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: index * 0.05 }}
+          viewport={{ once: true }}
+          className="grid"
+        >
+          <BoardCard image={item.image} name={item.name} role={item.role} />
+        </motion.div>
       ))}
     </div>
   );
