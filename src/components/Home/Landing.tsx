7beefbfec1d5df7efc3b32e93a87ba97fe1162ab
Peyton Chen <pchen215@ucr.edu>
2025-05-21 12:42:10 -0700

    Add animation

--- src/components/Home/Landing.tsx
+++ src/components/Home/Landing.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Image from "next/image";
 import Umbrella from "@/public/assets/nsu0.webp";
 import Lanterns from "@/public/assets/lanterns.webp";
@@ -11,7 +13,13 @@ const Landing = () => {
             <Image src={Umbrella} alt="NSU Umbrella" />
           </div>
           <div className="flex flex-col md:space-y-3 md:pt-8 lg:pt-20">
-            <div className="mt-10 ml-[12vw] text-4xl font-extrabold md:text-6xl lg:text-8xl">
+            <motion.div
+              initial={{ x: -200 }}
+              whileInView={{ x: 0 }}
+              transition={{ duration: 0.25 }}
+              viewport={{ once: true }}
+              className="mt-10 ml-[12vw] text-4xl font-extrabold md:text-6xl lg:text-8xl"
+            >
               <h1
                 className="text-nsu-tan-100 absolute z-0 mt-[2px] ml-[2px] lg:mt-1 lg:ml-1"
                 style={{ WebkitTextStroke: "1px #70161e" }}
@@ -19,8 +27,14 @@ const Landing = () => {
                 Nikkei
               </h1>
               <h1 className="text-nsu-red-200 relative z-10">Nikkei</h1>
-            </div>
-            <div className="ml-15 ml-[22vw] text-4xl font-extrabold md:text-6xl lg:text-8xl">
+            </motion.div>
+            <motion.div
+              initial={{ x: -200 }}
+              whileInView={{ x: 0 }}
+              transition={{ duration: 0.25 }}
+              viewport={{ once: true }}
+              className="ml-15 ml-[22vw] text-4xl font-extrabold md:text-6xl lg:text-8xl"
+            >
               <h1
                 className="text-nsu-tan-100 absolute z-0 mt-[2px] ml-[2px] lg:mt-1 lg:ml-1"
                 style={{ WebkitTextStroke: "1px #70161e" }}
@@ -28,8 +42,14 @@ const Landing = () => {
                 Student
               </h1>
               <h1 className="text-nsu-red-200 relative z-10">Student</h1>
-            </div>
-            <div className="ml-35 ml-[37vw] text-4xl font-extrabold md:text-6xl lg:text-8xl">
+            </motion.div>
+            <motion.div
+              initial={{ x: -200 }}
+              whileInView={{ x: 0 }}
+              transition={{ duration: 0.25 }}
+              viewport={{ once: true }}
+              className="ml-35 ml-[37vw] text-4xl font-extrabold md:text-6xl lg:text-8xl"
+            >
               <h1
                 className="text-nsu-tan-100 absolute z-0 mt-[2px] ml-[2px] lg:mt-1 lg:ml-1"
                 style={{ WebkitTextStroke: "1px #70161e" }}
@@ -37,20 +57,30 @@ const Landing = () => {
                 Union
               </h1>
               <h1 className="text-nsu-red-200 relative z-10">Union</h1>
-            </div>
+            </motion.div>
           </div>
         </div>
-        <div className="flex flex-[3]">
+        <motion.div
+          initial={{ y: "-50%" }}
+          whileInView={{ y: "0%" }}
+          viewport={{ once: true }}
+          className="relative z-0 flex flex-[3]"
+        >
           <Image
             src={Lanterns}
             alt="NSU Lanterns"
             className="w-full object-contain object-top"
           />
-        </div>
+        </motion.div>
       </div>
-      <div className="text-nsu-black-100 border-nsu-red-200 mx-[6vw] mb-5 border-b-4 py-2 text-center text-[0.80rem] md:mt-5 md:text-[1.50rem] lg:ml-20 lg:w-9/16 xl:text-[1.68rem]">
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        viewport={{ once: true }}
+        className="text-nsu-black-100 border-nsu-red-200 mx-[6vw] mb-5 border-b-4 py-2 text-center text-[0.80rem] md:mt-5 md:text-[1.50rem] lg:ml-20 lg:w-9/16 xl:text-[1.68rem]"
+      >
         NSU is a diverse Japanese socio-cultural club at UCR.
-      </div>
+      </motion.div>
     </div>
   );
 };
