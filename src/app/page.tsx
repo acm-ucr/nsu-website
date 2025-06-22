c2081c0bea020e26c5d987c9bda71551aaa96342
Kolzza <cole.miller2022@gmail.com>
2025-06-22 19:34:25 -0400

    locked in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import Welcome from "@/components/home/Welcome";
 import Offer from "@/components/home/Offer";
 import AdditionalInfo from "@/components/home/AdditionalInfo";
 import JoinUsButton from "@/components/home/JoinUsButton";
-import { motion } from "motion/react";
-
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
