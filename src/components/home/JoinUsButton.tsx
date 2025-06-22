ab715a2aec954c273f6734a5da7ff6eb0c70d35b
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2025-06-21 20:41:03 -0700

    reponsify join us button and also optimized welcome.tsx

--- src/components/home/JoinUsButton.tsx
+++ src/components/home/JoinUsButton.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 const JoinUsButton = () => {
   return (
-    <div className="bg-nsu-red-200 flex w-1/6 items-center justify-center rounded-3xl p-5 duration-150 hover:scale-110">
-      <Link href="/404" className="text-5xl font-bold text-white">
+    <div className="bg-nsu-red-200 flex w-1/3 items-center justify-center rounded-3xl p-3 duration-150 hover:scale-110 md:w-1/4 lg:w-1/5 lg:p-5 xl:w-1/6">
+      <Link
+        href="/404"
+        className="text-2xl font-bold text-white md:text-3xl lg:text-4xl xl:text-5xl"
+      >
         JOIN US
       </Link>
     </div>
